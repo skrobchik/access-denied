b2d5d2692f0bf0f10b280881b87c85180542a5eb
Robert <sk.robchik@gmail.com>
2021-10-11 19:51:40 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var root = document.documentElement;
-var eyef = document.getElementById('eyef');
-var cx = document.getElementById("eyef").getAttribute("cx");
-var cy = document.getElementById("eyef").getAttribute("cy");
-
-document.addEventListener("mousemove", evt => {
-  let x = evt.clientX / innerWidth;
-  let y = evt.clientY / innerHeight;
-
-  root.style.setProperty("--mouse-x", x);
-  root.style.setProperty("--mouse-y", y);
-  
-  cx = 115 + 30 * x;
-  cy = 50 + 30 * y;
-  eyef.setAttribute("cx", cx);
-  eyef.setAttribute("cy", cy);
-  
-});
-
-document.addEventListener("touchmove", touchHandler => {
-  let x = touchHandler.touches[0].clientX / innerWidth;
-  let y = touchHandler.touches[0].clientY / innerHeight;
-
-  root.style.setProperty("--mouse-x", x);
-  root.style.setProperty("--mouse-y", y);
-});
+// animated this about two weeks ago but had no chance posting it. 
+// i don't own this svg drawing. 
+// SVG drawing is by : http://bit.ly/2M799CT
